5028b30174fc9957bad32a6b3ab5a2720a7f37f9
EliseAur <94222653+EliseAur@users.noreply.github.com>
2025-08-25 21:05:22 +0200

    Removes unused import

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,4 +1,3 @@
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
