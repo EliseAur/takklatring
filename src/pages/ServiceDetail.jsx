e3c624ce502384dc8ab2c3b3ccecc112c5259493
EliseAur <94222653+EliseAur@users.noreply.github.com>
2026-02-21 14:27:37 +0100

    ServiceDetail hero changed to grid columns on large screens so that it is easier to find suitable images for the featured image in wordpress.

--- src/pages/ServiceDetail.jsx
+++ src/pages/ServiceDetail.jsx
@@ -23,68 +23,54 @@ export default function ServiceDetail() {
   if (!service) return null;
 
   return (
-    <main className="">
+    <main>
       {/* Hero / header */}
       <section className="bg-darkblue">
-        <div className="max-w-4xl mx-auto px-4 py-12">
-          <p className="text-orange font-bold uppercase tracking-wide">Tjeneste</p>
-          <h1 className="text-4xl md:text-5xl font-headings font-bold text-neutral-100 mt-2">{service.title}</h1>
-          <div className="mt-4 h-1 w-20 bg-orange rounded-sm" />
+        <div className="max-w-4xl mx-auto px-6 md:px-0 py-12 lg:py-16 grid gap-5 lg:grid-cols-2 lg:items-center">
+          {/* Text */}
+          <div className="max-w-4xl mx-auto md:px-14 lg:px-4 sm:py-6 lg:pt-0">
+            <p className="text-orange font-bold uppercase tracking-wide">Tjeneste</p>
+            <h1 className="text-4xl md:text-5xl font-headings font-bold text-neutral-100 mt-2">{service.title}</h1>
+            <div className="mt-4 h-1 w-20 bg-orange rounded-sm" />
 
-          {service?.acf?.tjeneste_subtitle1 && <p className="mt-4 text-lg text-neutral-300 max-w-2xl">{service.acf.tjeneste_short_description}</p>}
+            {service?.acf?.tjeneste_short_description && <p className="mt-4 text-lg text-neutral-300">{service.acf.tjeneste_short_description}</p>}
 
-          <div className="mt-8 flex gap-2 md:gap-3 max-w-sm">
-            {/* <a className="bg-orange text-darkblue font-headings uppercase font-bold px-6 py-3 rounded-sm" href="/kontakt">
-              Bestill befaring
-            </a> */}
-            <a
-              className="flex-1 text-center bg-orange text-darkblue font-headings uppercase text-sm lg:text-lg font-bold py-3 px-6 rounded-sm hover:cursor-pointer drop-shadow-[0_1px_2px_rgba(0,0,0,0.7)] hover:shadow-xl hover:scale-[1.01]"
-              href="/kontakt"
-            >
-              Få tilbud
+            <a href="#content" className="mt-4 inline-block text-white font-bold hover:pointer-cursor">
+              Les mer →
             </a>
 
-            <a
-              className="flex-1 text-center border border-neutral-100 text-neutral-100 font-headings font-bold uppercase text-sm lg:text-lg px-6 py-3 rounded-sm hover:cursor-pointer drop-shadow-[0_1px_2px_rgba(0,0,0,0.7)] hover:shadow-xl hover:scale-[1.01]"
-              href="/tjenester"
-            >
-              Alle tjenester
-            </a>
+            <div className="mt-8 flex gap-2 md:gap-3">
+              <a
+                className="flex-1 text-center bg-orange text-darkblue font-headings uppercase text-sm lg:text-lg font-bold py-3 px-6 rounded-sm hover:cursor-pointer drop-shadow-[0_1px_2px_rgba(0,0,0,0.7)] hover:shadow-xl hover:scale-[1.01]"
+                href="/kontakt"
+              >
+                Få tilbud
+              </a>
+
+              <a
+                className="flex-1 text-center border border-neutral-100 text-neutral-100 font-headings font-bold uppercase text-sm lg:text-lg px-6 py-3 rounded-sm hover:cursor-pointer drop-shadow-[0_1px_2px_rgba(0,0,0,0.7)] hover:shadow-xl hover:scale-[1.01]"
+                href="/tjenester"
+              >
+                Alle tjenester
+              </a>
+            </div>
           </div>
+          {/* Image */}
+          {service.image_url && (
+            <div className="overflow-hidden rounded-md shadow-md md:px-14 lg:px-4">
+              <img src={service.image_url} alt={service.image_alt || ""} className="w-full aspect-[4/3] sm:aspect-[3/4] object-cover object-center max-h-[500px]" />
+            </div>
+          )}
         </div>
       </section>
 
-      {/* Featured image */}
-      {service.image_url && (
-        // <section className=" mx-auto my-12 max-w-6xl px-6">
-        <section className=" mx-auto">
-          <div className="overflow-hidden relative">
-            <img src={service.image_url} alt={service.image_alt || ""} className="w-full h-[320px] md:h-[420px] object-cover" />
-            {/* subtil overlay for å gi mer “premium” look */}
-            <div className="absolute inset-0 bg-gradient-to-b from-darkblue/30 via-transparent to-darkblue/20" />
-          </div>
-        </section>
-      )}
-
       {/* Content */}
-      <section className="w-full">
-        <div className="max-w-6xl mx-auto px-3 pt-0 pb-6">
-          <article className="wp-content max-w-4xl mx-auto px-3 md:px-10">
+      <section id="content" className="scroll-mt-[125px] lg:scroll-mt-[100px] w-full bg-neutral-100">
+        <div className="max-w-6xl mx-auto px-6 pt-0 lg:pt-6 pb-6">
+          <article className="wp-content max-w-4xl mx-auto md:px-14 my-10 lg:px-4">
             <div dangerouslySetInnerHTML={{ __html: service.content }} />
           </article>
         </div>
-        {/* <div className="w-full px-3 mx-auto bg-darkblue">
-          <div className="px-3 md:px-10 max-w-4xl mx-auto py-10">
-            <h2 className="text-3xl md:text-4xl font-headings font-bold text-neutral-100 mb-4">Ønsker du et uforpliktende tilbud?</h2>
-            <p className="mt-2 text-neutral-300 text-lg">Send oss en kort beskrivelse, så tar vi kontakt og finner riktig løsning for deg.</p>
-            <a
-              href="/kontakt"
-              className="text-sm lg:text-lg mt-5 inline-block text-center bg-orange text-darkblue font-headings uppercase font-bold py-3 px-12 rounded-sm hover:cursor-pointer drop-shadow-[0_1px_2px_rgba(0,0,0,0.7)] hover:shadow-xl hover:scale-[1.01]"
-            >
-              Få tilbud
-            </a>
-          </div>
-        </div> */}
       </section>
       <BookingSection />
       <ReviewsSection />
