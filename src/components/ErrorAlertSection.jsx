9185665b7a5a56a5bebcd519204b6216a15f4e78
EliseAur <94222653+EliseAur@users.noreply.github.com>
2026-04-08 16:23:51 +0200

    style: add theme colors for background, border and text in error and success messages

--- src/components/ErrorAlertSection.jsx
+++ src/components/ErrorAlertSection.jsx
@@ -1,8 +1,8 @@
 export default function ErrorAlertSection({ message }) {
   return (
     <div className="max-w-6xl mx-auto">
-      <div className="rounded-md border-l-4 border-red-400 bg-errorBg px-5 py-4">
-        <p className="text-red-700 font-medium">{message}</p>
+      <div className="rounded-md border-l-4 border-errorBorder bg-errorBg px-5 py-4">
+        <p className="text-errorText font-medium">{message}</p>
       </div>
     </div>
   );
